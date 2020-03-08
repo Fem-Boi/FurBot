488b7db964ceb4fb46bd147a801d703d0206954b
Fem-Boi <58786791+Fem-Boi@users.noreply.github.com>
2020-03-08 14:48:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const moment = require('moment');
 
 if (process.env.NODE_ENV === 'development') {
   console.log(`==============================================================================`);
-  console.log(chalk.magenta.bold(`[FurBot]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting dev...`);
+  console.log(chalk.magenta.bold(`[Furry]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting dev...`);
   require('babel-register');
   require('babel-polyfill');
   require('./src/');
 } else {
   console.log(`==============================================================================`);
-  console.log(chalk.magenta.bold(`[FurBot]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting...`);
+  console.log(chalk.magenta.bold(`[Furry]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting...`);
   require('babel-polyfill');
   require('./dist');
 }
