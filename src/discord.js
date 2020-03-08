e1ef353c010262ffe49161cf6ce64db373956e5e
Fem-Boi <58786791+Fem-Boi@users.noreply.github.com>
2020-03-08 14:59:59 -0400

    Update discord.js

--- src/discord.js
+++ src/discord.js
@@ -272,3 +272,25 @@ export function start() {
   startPortalIntervals(client);
   connect();
 }
+const Discord = require("discord.js");
+const Cleverbot = require("cleverbot-node");
+const client = new Discord.Client();
+const clbot = new Cleverbot;
+ 
+client.on("message", message => {
+  if (message.channel.type === "dm") {
+    clbot.write(message.content, (response) => {
+      message.channel.startTyping();
+      setTimeout(() => {
+        message.channel.send(response.output).catch(console.error);
+        message.channel.stopTyping();
+      }, Math.random() * (1 - 3) + 1 * 1000);
+    });
+  }
+});
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.login("superSecretBotTokenHere");
